e9405273ebd5a82d9b679796d6391a726a34aa01
Mufaddal Patanwala <65411218+Mufaddal1125@users.noreply.github.com>
2024-10-25 19:50:45 +0530

    fix cors issue

--- content_chronicle/settings.py
+++ content_chronicle/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 import dj_database_url
+from corsheaders.middleware import CorsMiddleware
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
